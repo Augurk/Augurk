e75077cd1e1a966bffb982b8db7b378e4ab84c51
Mark Taling <markt@infosupport.com>
2015-09-30 11:07:41 +0200

    Sorted the products (formerly branches) in an alphabetic order (based on unicode).
    Resolves #7

--- src/Augurk.Entities/Properties/AssemblyInfo.cs
+++ src/Augurk.Entities/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -21,17 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9c902ba1-00f2-4796-ad45-e66b3a2a0e07")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
