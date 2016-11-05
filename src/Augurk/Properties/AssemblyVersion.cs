1ab64611cc7daee407192fbe723f948beaadbc72
marktaling <mark@taling.eu>
2016-11-05 12:03:23 +0100

    Updated Packages Fixed remaining reference to branches to now refer to product Disabled removal of server tags from scenarios

--- src/Augurk/Properties/AssemblyVersion.cs
+++ src/Augurk/Properties/AssemblyVersion.cs
@@ -9,6 +9,6 @@
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.3.1")]
-[assembly: AssemblyFileVersion("2.3.1")]
-[assembly: AssemblyInformationalVersion("2.3.1")]
+[assembly: AssemblyVersion("2.3.2")]
+[assembly: AssemblyFileVersion("2.3.2")]
+[assembly: AssemblyInformationalVersion("2.3.2")]
