fb6cc4597670d8a928c8f9dc6ced8b0d36372211
Jonathan Mezach <jonathanm@infosupport.com>
2018-11-08 20:07:56 +0100

    Add script for publishing on non-Windows platforms Add config files for VSCode Fix compile error for Augurk.Test project

---
+++ publish.sh
@@ -0,0 +1,5 @@
+augurk publish --featureFiles "$PWD/src/Augurk.Specifications/Gherkin" --productName Documentation --groupName Gherkin --url $1 --embed
+augurk publish --featureFiles "$PWD/src/Augurk.Specifications/Configuration" --productName Documentation --groupName Configuration --url $1 --embed
+
+augurk publish --featureFiles "$PWD/src/Augurk.Specifications/Portal\Versioned Features\V1" --productName Documentation --groupName Portal --version 1.0.0 --url $1 --embed
+augurk publish --featureFiles "$PWD/src/Augurk.Specifications/Portal\Versioned Features\V2" --productName Documentation --groupName Portal --version 2.0.0 --url $1 --embed
