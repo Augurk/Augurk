1a1efc154a7f127b42f94dc6f556eee84d135d13
Jonathan Mezach <jonathanm@infosupport.com>
2019-02-12 21:23:57 +0100

    Fix Mark's vinegar

--- src/Augurk/Controllers/V1/FeatureController.cs
+++ src/Augurk/Controllers/V1/FeatureController.cs
@@ -1,5 +1,5 @@
 ﻿/*
- Copyright 2014-2015, Mark Taling
+ Copyright 2014-2019, Augurk
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
--- src/Augurk/DataAccess/DbInvocation.cs
+++ src/Augurk/DataAccess/DbInvocation.cs
@@ -1,5 +1,5 @@
 ﻿/*
- Copyright 2018, Augurk
+ Copyright 2018-2019, Augurk
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
