6dd5d1c22d5f315c95b2b9de3870641cc9c157f2
Mark Taling <mark@taling.eu>
2020-04-28 14:55:49 +0200

    Added some styling

--- src/Augurk.UI/Services/SearchService.cs
+++ src/Augurk.UI/Services/SearchService.cs
@@ -3,9 +3,6 @@ using System.Threading.Tasks;
 using Augurk.Entities.Search;
 
 public class SearchService {
-
-    private string _searchInput;
-
     public SearchResults LatestResults {
         get; private set;
     }
@@ -14,19 +11,11 @@ public class SearchService {
 
     public async Task Search(string searchInput)
     {
-        _searchInput = searchInput;
-
-        await Task.Delay(500);
-
-        // Don't do anything if the input has changed
-        if(_searchInput != searchInput)
-        {
-            return;
-        }
-
         var results = new SearchResults() {
             SearchQuery = searchInput,
-            FeatureMatches = new [] { new FeatureMatch{ FeatureName = "Feature 1" }, new FeatureMatch{ FeatureName = "Feature 2" }}
+            FeatureMatches = new [] {
+                new FeatureMatch{ FeatureName = "Feature 1", MatchingText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis dui sed eros consectetur aliquet. Donec varius libero ligula, ut fermentum tellus porttitor non." },
+                new FeatureMatch{ FeatureName = "Feature with a very long name which mostly will not fit the screen", MatchingText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis dui sed eros consectetur aliquet. Donec varius libero ligula, ut fermentum tellus porttitor non." }}
         };
 
         LatestResults = results;
