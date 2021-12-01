00118fd74fb3a42eec8c393eda56992d2931339a
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2021-12-01 08:52:23 +0100

    Improve styling for feature page (#385)
    * Fix styling on tables
    
    * Implemented links to other features
    
    * Sticky top
    
    * Add toggle switch for collapsing all scenarios
    
    * Remove About link
    
    * Arbitrary fixes
    
    * Added preview link
    
    * Handle not found features more gracefully
    
    * Fix some styling issues
    
    * Change css class
    
    * Add missing div
    
    * Remove some useless whitespace
    
    * Altered title handling
    
    * Added some comments to clarify stuff
    
    Co-authored-by: Mark Taling <mark@taling.eu>

--- src/Augurk.UI/Components/Markdown.cs
+++ src/Augurk.UI/Components/Markdown.cs
@@ -1,16 +1,27 @@
 // Copyright (c) Augurk. All Rights Reserved.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
+using System.Globalization;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
 using Markdig;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Augurk.UI.Components
 {
+    public delegate string HandleLink(string linkText);
+
     public class Markdown : ComponentBase
     {
         [Parameter]
         public string Input { get; set; }
+
+        [Parameter]
+        public HandleLink LinkHandler { get; set; }
+
         private string _rawHtml = string.Empty;
 
         public virtual MarkdownPipeline Pipeline => new MarkdownPipelineBuilder()
@@ -28,12 +39,35 @@ namespace Augurk.UI.Components
             base.OnParametersSet();
             if (!string.IsNullOrEmpty(Input))
             {
-                _rawHtml = Markdig.Markdown.ToHtml(Input, Pipeline);
+                _rawHtml = Markdig.Markdown.ToHtml(ParseFeatureLinks(Input), Pipeline);
             }
             else
             {
                 _rawHtml = string.Empty;
             }
+        }
+
+        private string ParseFeatureLinks(string rawContent)
+        {
+            const string regex = @"\[(?<feature>[^\]]+)\]\z|\[(?<feature>[^\]]+)\](?=[^\(])";
+            StringBuilder sb = new StringBuilder();
+            int prevPosition = 0;
+            var lh = LinkHandler;
+            if(lh != null)
+            {
+                foreach(Match match in Regex.Matches(rawContent, regex))
+                {
+                    sb.Append(rawContent.Substring(prevPosition, match.Index - prevPosition));
+                    sb.Append(match.Value);
+                    // Only encode the whitespaces, as the markdown handler does not appreciate them
+                    sb.AppendFormat(CultureInfo.InvariantCulture, "({0})", lh(match.Groups["feature"].Value).Replace(" ", "%20"));
+
+                    prevPosition = match.Index + match.Length;
+                }
+            }
+            sb.Append(rawContent.Substring(prevPosition));
+
+            return sb.ToString();
         }
     }
 }
