e5db84379b9d4e3b9a84cc2f14e225ffbff09096
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2021-12-08 15:11:19 +0100

    Fix a bug with no version regex Make configuration saving work

--- src/Augurk.Entities/Configuration.cs
+++ src/Augurk.Entities/Configuration.cs
@@ -12,24 +12,24 @@ namespace Augurk.Entities
         /// <summary>
         /// Indicates whether the expiration of features is enabled.
         /// </summary>
-        public bool ExpirationEnabled;
+        public bool ExpirationEnabled { get; set; }
 
         /// <summary>
         /// Defines the number of days after which an features should expire.
         /// </summary>
-        public int ExpirationDays;
+        public int ExpirationDays { get; set; }
 
         /// <summary>
         /// Defines the regular expression which a feature version must match to expire.
         /// </summary>
-        public string ExpirationRegex;
+        public string ExpirationRegex { get; set; }
         #endregion
 
         #region Dependencies
         /// <summary>
         /// Indicates whether the analysis of dependencies is enabled.
         /// </summary>
-        public bool DependenciesEnabled;
+        public bool DependenciesEnabled { get; set; }
         #endregion
     }
 }
